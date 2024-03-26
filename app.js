7bf41723fb85205df87e62eaef57d505775fc6b4
Saharat Kaewmanee <saharat35293@gmail.com>
2024-03-26 23:56:59 +0700

    Add Template
    Add Template

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require("express");
 const chalk = require("chalk");
 const debug = require("debug")("app");
 const morgan = require("morgan");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")));
 
 app.get("/", (req, res) => {
-  res.send("Hello Fluk!!!!");
+  res.send("Hello Fluk!");
 });
 app.listen(port, () => {
   debug("Listening on Port " + chalk.green(port));
